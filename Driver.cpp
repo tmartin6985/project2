4ade6e8994e571cd0a82394028d9666fc1c466d3
mattnokes5150 <mnokes42@tntech.edu>
2024-11-07 08:19:45 -0600

    Updated Driver.cpp with basic menu and added include statements

--- Driver.cpp
+++ Driver.cpp
@@ -1,1 +1,28 @@
-// this is the driver
+/*
+Project: CSC 1310 Lab Program 2: Parking Lot Manager
+File: Driver.cpp
+Authors: Tristen Martin, Matt Nokes
+Date: 11.5.2024
+*/
+
+#include "DataClass.h"
+#include "LinkedList.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int userMenuOption = 0; 
+
+    do{
+        cout << "Welcome to the Tennessee Tech Parking Lot Manager\n";
+        cout << "Pick an option:\n";
+        cout << "1.\tView Cars in Parking Lot\n";
+        cout << "2.\tAdd Car to Parking Lot\n";
+        cout << "3.\tRemove Car from Parking Lot\n";
+        cout << "4.\tExit Program\n";
+        cout << "CHOICE: ";
+        cin >> userMenuOption;
+    } while(userMenuOption != 4) // I chose 4 as the exit option but this can be changed at any time
+
+    return 0;
+}
