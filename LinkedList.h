384976999c135ba64d8bbe4ef621c1ed26458717
mattnokes5150 <mnokes42@tntech.edu>
2024-12-05 16:39:06 -0600

    Big update to the LinkedList header file

--- LinkedList.h
+++ LinkedList.h
@@ -1,29 +1,105 @@
-// this is the linked list class header file
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-class LinkedList
-{
-	private:
-		struct ListNode
-		{
-			double value;           	//value node
-			struct ListNode *next;  	//pointer to next node
-		}; 
+#include <iostream>
+using namespace std;
 
-		ListNode *head;		//list head pointer
-		ListNode *tail;		//list tail pointer
+template <typename T>
+class Node {
+public:
+    T* data;
+    Node* next;
 
-	public:
-		//constructor
-		LinkedList()
-		{ 
-			head = NULL; 
-			tail = NULL;
-		}
-
-		// Destructor (deallocates all nodes)
-		~LinkedList();
+    Node(T* data) {
+        this->data = data;
+        this->next = nullptr;
+    }
 };
 
-#endif
+template <typename T>
+class LinkedList {
+private:
+    Node<T>* head;
+    int size;
+
+public:
+    LinkedList() {
+        head = nullptr;
+        size = 0;
+    }
+
+    void add(T* item) {
+        Node<T>* newNode = new Node<T>(item);
+        if (!head) {
+            head = newNode;
+        } else {
+            Node<T>* temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+        size++;
+    }
+
+    void display() const {
+        Node<T>* temp = head;
+        while (temp) {
+            cout << *(temp->data) << endl;
+            temp = temp->next;
+        }
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    T* get(int index) const {
+        if (index < 0 || index >= size) {
+            return nullptr;
+        }
+
+        Node<T>* temp = head;
+        for (int i = 0; i < index; ++i) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    void remove(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        Node<T>* temp = head;
+        if (index == 0) {
+            head = head->next;
+            delete temp->data;
+            delete temp;
+            size--;
+            return;
+        }
+
+        for (int i = 0; i < index - 1; ++i) {
+            temp = temp->next;
+        }
+
+        Node<T>* toDelete = temp->next;
+        temp->next = toDelete->next;
+        delete toDelete->data;
+        delete toDelete;
+        size--;
+    }
+
+    ~LinkedList() {
+        Node<T>* temp = head;
+        while (temp) {
+            Node<T>* next = temp->next;
+            delete temp->data;
+            delete temp;
+            temp = next;
+        }
+    }
+};
+
+#endif // LINKEDLIST_H
