29c6339b8bd8ae54a6f4ec72ff513cd4b0836ca1
mattnokes5150 <mnokes42@tntech.edu>
2024-12-11 10:45:45 -0600

    ParkingLot.h now correpsonds to new ParkingLot.cpp file

--- ParkingLot.h
+++ ParkingLot.h
@@ -16,32 +16,9 @@ public:
         cout << "Constructor called for Parking Lot" << endl; //for bug testing
     }
 
-    void addCar(const string& plate) {
-         cars.push_back(SmartPointer<Car>(new Car(plate)));  // Dynamically allocate cars
-    }
-
-    void removeCar(const string& plate) {
-        for (auto it = cars.begin(); it != cars.end();) {
-            if ((*it)->getLicensePlate() == plate) {
-                cars.erase(it);  // Remove it from the vector
-                return;
-            } else {
-                ++it; //increments if there are no errors
-            }
-        }
-
-        cout << "Car with plate " << plate << " not found.\n";
-    }
-
-    void displayCars() const {
-        if (cars.empty()) {
-            cout << "No cars in the parking lot.\n";
-        } else {
-            for (const auto& car : cars) {
-                cout << car << endl;
-            }
-        }
-    }
+    void addCar(const string& plate);
+    void removeCar(const string& plate);
+    void displayCars() const;
 
     friend ostream& operator << (ostream& os, const ParkingLot& lot) {
         os << "Parking Lot [Cars: " << lot.cars.size() << "]";
