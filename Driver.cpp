160a7c14c85d7d7a6b907a2954fc729d72e24d5f
mattnokes5150 <mnokes42@tntech.edu>
2024-12-12 17:01:43 -0600

    Added bubble sort option and car make, model, and color attributes

--- Driver.cpp
+++ Driver.cpp
@@ -15,7 +15,7 @@ int main(){
     int userMenuOption = 0; 
     int numParkingLots = 0; // might be used in a future print statement
     int lotIndex;
-    string plate;
+    string plate, color, make, model, carAttribute;
     LinkedList<ParkingLot> parkingLots;
 
     cout << "Welcome to the Tennessee Tech Parking Lot Manager\n";
@@ -27,7 +27,8 @@ int main(){
         cout << "2. View Cars in Parking Lot\n";
         cout << "3. Add Car to Parking Lot\n";
         cout << "4. Remove Car from Parking Lot\n";
-        cout << "5. Exit Program\n";
+        cout << "5. Sort Cars in Parking Lot\n";
+        cout << "6. Exit Program\n";
         cout << "CHOICE: ";
         cin >> userMenuOption;
 
@@ -46,7 +47,7 @@ int main(){
             
             else{
                 for(int i = 0; i < parkingLots.getSize(); i++){
-                    cout << "Parking Lot " << i + 1 << ":\n";
+                    cout << "\nParking Lot " << i + 1 << ":\n";
                     parkingLots.get(i)->displayCars();
                 }
             }
@@ -57,21 +58,29 @@ int main(){
                 cout << "No parking lots available. Create at least one parking lot and one car first.\n";
             }
 
-            else{
+            else {
                 do {
                     cout << "Enter parking lot index (1 to " << parkingLots.getSize() << "): ";
                     cin >> lotIndex;
+                    cin.ignore();
 
-                    if (lotIndex < 1 || lotIndex > parkingLots.getSize()){
+                    if (lotIndex < 1 || lotIndex > parkingLots.getSize()) {
                         cout << "Invalid parking lot index.\n";
                     }
                 } while (lotIndex < 1 || lotIndex > parkingLots.getSize());
 
                 cout << "Enter the car's license plate: ";
-                cin >> plate;
-                parkingLots.get(lotIndex - 1)->addCar(plate);
+                getline(cin, plate);
+                cout << "Enter the car's make: ";
+                getline(cin, make);
+                cout << "Enter the car's model: ";
+                getline(cin, model);
+                cout << "Enter the car's color: ";
+                getline(cin, color);
+
+                parkingLots.get(lotIndex - 1)->addCar(plate, color, make, model);
                 cout << "Car added.\n";
-            }
+    }
         } 
         
         else if(userMenuOption == 4){ // remove car from parking lot
@@ -82,7 +91,7 @@ int main(){
             else{
                 cout << "Available parking lots: " << parkingLots.getSize() << endl; // Debug
                 for (int i = 0; i < parkingLots.getSize(); ++i) {
-                    cout << "Parking Lot " << i + 1 << ":\n";
+                    cout << "\nParking Lot " << i + 1 << ":\n";
                     parkingLots.get(i)->displayCars();
                 }
 
@@ -90,35 +99,53 @@ int main(){
                     cout << "No parking lots available. Create at least one parking lot and one car first.\n";
                 }
 
-                else if(parkingLots.get(0)->displayCars() == false){
-                    cout << "No cars in the parking lot.\n";
-                }
-
                 else{
                     do{
                         cout << "Enter parking lot index (1 to " << parkingLots.getSize() << "): ";
                         cin >> lotIndex;
-
                         if (lotIndex < 1 || lotIndex > parkingLots.getSize()){
                             cout << "Invalid parking lot index.\n";
                         }
                     } while (lotIndex < 1 || lotIndex > parkingLots.getSize());
                     cout << "Enter the car's license plate to remove: ";
-                    cin >> plate;
+                    getline(cin, plate);
                     parkingLots.get(lotIndex - 1) -> removeCar(plate);
                 }
             }
         }
         
-        else if(userMenuOption == 5){
+        else if(userMenuOption == 5){ // bubble sort cars in parking lot
+            if(parkingLots.getSize() <= 0){
+                cout << "No parking lots available. Create at least one parking lot and one car first.\n";
+            } 
+            
+            else{
+                do{
+                    cout << "Enter parking lot index (1 to " << parkingLots.getSize() << "): ";
+                    cin >> lotIndex;
+                    cin.ignore();
+
+                    if(lotIndex < 1 || lotIndex > parkingLots.getSize()){
+                        cout << "Invalid parking lot index.\n";
+                    }
+                } while (lotIndex < 1 || lotIndex > parkingLots.getSize());
+
+                cout << "Enter the car attribute to sort by ('make', 'model', or 'color'): ";
+                cin >> carAttribute;
+                parkingLots.get(lotIndex - 1)->sortCars(carAttribute);
+                cout << "Cars sorted.\n";
+            }
+        } 
+
+        else if(userMenuOption == 6){ // exit program
             cout << "Exiting Program...\n";
         } 
         
-        else {
+        else{
             cout << "Invalid Option: " << userMenuOption << "\n";
             userMenuOption = 0;
         }
 
-    } while(userMenuOption != 5);
+    } while(userMenuOption != 6);
     return 0;
 }
