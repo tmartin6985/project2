091472394824b7e42411a018d3e284fd78131f2e
mattnokes5150 <mnokes42@tntech.edu>
2024-12-12 17:03:42 -0600

    Added bubble sort option and car make, model, and color attributes

--- ParkingLot.cpp
+++ ParkingLot.cpp
@@ -1,31 +1,81 @@
 #include "ParkingLot.h"
+#include "smartpointer.h"
 using namespace std;
 
-void ParkingLot::addCar(const string& plate) {
-    cars.push_back(SmartPointer<Car>(new Car(plate)));  // Dynamically allocate cars
+ParkingLot::ParkingLot() {
+    cout << "ParkingLot constructor called.\n";
 }
 
-void ParkingLot::removeCar(const string& plate){
-    for (auto it = cars.begin(); it != cars.end();) {
+void ParkingLot::addCar(const string& plate, const string& color, const string& make, const string& model) {
+    cars.push_back(SmartPointer<Car>(new Car(plate, color, make, model)));  // Add the new car
+    cout << "Car added with details [Plate: " << plate
+         << ", Color: " << color
+         << ", Make: " << make
+         << ", Model: " << model
+         << "]. Total cars: " << cars.size() << endl;
+}
+
+void ParkingLot::removeCar(const string& plate) {
+    vector<SmartPointer<Car>>::iterator it = cars.begin(); // variable "it" is the iterator
+    while (it != cars.end()) {
         if ((*it)->getLicensePlate() == plate) {
-            cars.erase(it);  // Remove it from the vector
+            it = cars.erase(it);  
+            cout << "Car with plate " << plate << " removed.\n";
             return;
         } 
-        else{
-            ++it; //increments if there are no errors
+
+        else {
+            it++;  
         }
     }
     cout << "Car with plate " << plate << " not found.\n";
 }
 
-void ParkingLot::displayCars() const{
+void ParkingLot::displayCars() const {
     if (cars.empty()) {
-            cout << "No cars in the parking lot.\n";
+        cout << "No cars in the parking lot.\n";
+        return;
     }
 
-    else{
-        for(int i = 0; i < cars.size(); i++){
-            cout << "Car " << i + 1 << ": " << cars[i] << endl;
+    for (int i = 0; i < cars.size(); i++) {
+        if (cars[i].operator->() != nullptr) {  
+            cout << "Car " << i + 1 << ": " << *(cars[i]) << endl;
+        } 
+        
+        else {
+            cout << "Car " << i + 1 << ": null pointer\n";
+        }
+    }
+}
+
+void ParkingLot::sortCars(const string& carAttribute) { // bubble sort algorithm
+    for (size_t i = 0; i < cars.size(); ++i) {
+        for (size_t j = 0; j < cars.size() - i - 1; ++j) {
+            bool swap = false;
+
+            if (carAttribute == "make"){
+                swap = cars[j]->getMake() > cars[j + 1]->getMake();
+            } 
+            
+            else if (carAttribute == "model"){
+                swap = cars[j]->getModel() > cars[j + 1]->getModel();
+            }
+            
+            else if (carAttribute == "color"){
+                swap = cars[j]->getColor() > cars[j + 1]->getColor();
+            }
+            
+            else {
+                cout << "Invalid sort attribute. Choose 'make', 'model', or 'color'.\n";
+                return;
+            }
+
+            if (swap){ 
+                // swaps cars[j] and cars[j + 1]
+                SmartPointer<Car> temp = cars[j];
+                cars[j] = cars[j + 1];
+                cars[j + 1] = temp;
+            }
         }
     }
 }
